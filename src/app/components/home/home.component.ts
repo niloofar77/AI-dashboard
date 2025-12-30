2ef8b2ae742965cf4c9c0f63ece02ba332db350d
niloofar <sn.mousavi77@gmail.com>
2025-12-30 17:53:26 +0330

    add header component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { ChatComponent } from '../features/chat/chat.component';
+import { HeaderCustomComponent } from '../shared/header-custom/header-custom.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent,ChatComponent],
+  imports: [SidebarComponent,ChatComponent,HeaderCustomComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
